525e20c4ac72a38a0c34994c8b2a2496c602b143
wxues <106729260+wxues@users.noreply.github.com>
2026-04-28 17:24:01 +0800

    Make reborn app executable

--- JianghuQunxiaZhuanReborn/App/JianghuQunxiaZhuanRebornApp.swift
+++ JianghuQunxiaZhuanReborn/App/JianghuQunxiaZhuanRebornApp.swift
@@ -1,9 +1,13 @@
 import SwiftUI
 
+@main
 struct JianghuQunxiaZhuanRebornApp: App {
+    @StateObject private var engine = JY3Engine()
+
     var body: some Scene {
         WindowGroup {
             GameRootView()
+                .environmentObject(engine)
         }
     }
 }
